2555388431bc213d70caa38fe2df69b7596a5a10
AnaCAROLINA39 <anacarolina93silva@gmail.com>
2024-04-18 20:50:38 -0300

    mudanças footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from "./moments/header/header.component";
+import { FooterComponent } from "./moments/footer/footer.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, HeaderComponent, FooterComponent]
 })
 export class AppModule {}
